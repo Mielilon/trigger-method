0eb3182d8392382d9b0d6c89a76835fd09a21737
Mielilon <daria.mg354@mail.ru>
2021-02-20 19:49:44 +0300

    Сделаны блоки: шапка, описание, работа

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 //= jquery.min.js
+//= owl.carousel.js
+
+var owl = $(".work__owl-carousel");
+owl.owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    touchDrag: false,
+    mouseDrag: false,
+    responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
