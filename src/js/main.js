22b7a64f726c20d6fc2878aa80b2d7c3e75f69be
Mielilon <daria.mg354@mail.ru>
2021-02-21 10:43:27 +0300

    Сделаны блоки: тарифы, сферы жизни

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 //= jquery.min.js
 //= owl.carousel.js
 
-var owl = $(".work__owl-carousel");
-owl.owlCarousel({
+$(".work__owl-carousel").owlCarousel({
     loop: false,
     margin: 10,
     nav: true,
@@ -17,6 +16,46 @@ owl.owlCarousel({
         },
         1000:{
             items:5
+        }
+    }
+});
+
+
+$(".tariffs__owl-carousel").owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: true,
+    touchDrag: false,
+    mouseDrag: false,
+    responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items: 3
+        }
+    }
+});
+
+
+$(".spheres-of-life__owl-carousel").owlCarousel({
+    loop: false,
+    margin: 27,
+    nav: true,
+    touchDrag: false,
+    mouseDrag: false,
+    responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items: 4
         }
     }
 });
