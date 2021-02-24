b68fd6bdea9db39be1937b91e55e554e5c220793
Mielilon <daria.mg354@mail.ru>
2021-02-24 17:31:10 +0300

    Изменена почта

--- build/mail.php
+++ build/mail.php
@@ -15,7 +15,7 @@ $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= "Content-type: text/html; charset=utf-8 \r\n";
 $headers .= "From: МЕТОД ТРИГГЕРА — психологический тренинг <metodtrigger@yandex.ru>\r\n";
 
-$recipient = "metodtrigger@yandex.ru";
+$recipient = "avtosity-as@yandex.ru";
 $subject = "Заявка МЕТОД ТРИГГЕРА";
 
 if(isset($name) && isset($phone)) {
